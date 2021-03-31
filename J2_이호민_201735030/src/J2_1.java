eb95f79ad1562cc06f498457821e53e49c95121c
iCAROS7 <hominlab@gmail.com>
2021-03-31 19:08:15 +0900

    과제 J2_1 추가

---
+++ J2_이호민_201735030/src/J2_1.java
@@ -0,0 +1,58 @@
+/*
+    이름 : 이호민
+    학번 : 201735030
+    문제 : J2_1
+
+    사용자로부터 x, y, z 3개 정수 입력 받아, 다음 문제 해결
+    - x부터 y까지 z의 배수에 속하는 수들의 합
+    - x부터 y까지 z의 배수를 제외한 수들의 합
+ */
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class J2_1 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] _input = new int[3]; // 0 = 시작, 1 = 종료, 2 = 배수
+
+        try {
+            System.out.print("시작 : ");
+            _input[0] = sc.nextInt();
+            System.out.print("종료 : ");
+            _input[1] = sc.nextInt();
+            System.out.print("배수 : ");
+            _input[2] = sc.nextInt();
+        }
+        catch (InputMismatchException e){
+            System.out.println("정상적이지 않은 값 입니다.");
+            System.exit(1);
+        }
+        catch (Exception e){
+            System.exit(1);
+        }
+        finally {
+            if ((_input[1] - _input[0]) <= 0 || _input[2] == 0) {
+                System.out.println("정상적이지 않은 값 입니다.");
+                System.exit(1);
+            }
+        }
+
+        int sumMulti = 0;
+        int sumNormal = 0;
+        for (int i = _input[0]; i <= _input[1]; i++){
+            if (i % _input[2] == 0){
+                sumMulti += i;
+            }
+            else{
+                sumNormal += i;
+            }
+        }
+
+        System.out.println( _input[0] + "부터 " + _input[1] + "까지 " + _input[2] + "의 배수에 속하는 수들의 합: " + sumMulti);
+        System.out.println( _input[0] + "부터 " + _input[1] + "까지 " + _input[2] + "의 배수를 제외한 수들의 합: " + sumNormal);
+    }
+}
